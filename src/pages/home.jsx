f588e8ad9ef7c480ef8fd1847608ce5f51b4b0e3
Optimistola <yakubshakirudeenolaide2018@gmail.com>
2022-12-26 05:37:42 -0800

    main

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,7 +8,7 @@ function Home(){
           <div className='md:flex mt-10 justify-around ml-5'>
             <div className='mt-5 md:ml-20 mr-50'>
             <h1 className='font-bold text-4xl md:text-6xl'><span className='text-red-600'>Th</span>e Art of speed food Quality</h1>
-            <p className=' font-bold mt-4 mb-8 mr-10 text-xl '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius sed pharetra dictum neque massa congue</p>
+            <p className='  mt-4 mb-8 mr-10 text-xl '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius sed pharetra dictum neque massa congue</p>
             <button className='bg-red-600 rounded-full  text-white py-3 px-5'><Link to='../menu'>See menu</Link></button>
             </div>
             <div className='md:mr-20 px-10'>
