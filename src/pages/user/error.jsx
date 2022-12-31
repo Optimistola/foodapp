04c4f1d36602361c9fa8a7c1677c413b438deb6e
Optimistola <yakubshakirudeenolaide2018@gmail.com>
2022-12-31 14:42:18 -0800

    main

--- src/pages/user/error.jsx
+++ src/pages/user/error.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 function Error(props){
 let {errortext}=props;
-    return <div className="bg-red-600 px-5 py-3 mt-20 text-white hidden" id='error'>{errortext}</div>;
+    return <div className="px-5 py-3 mt-20 text-red-600 text-bold " id='error'>{errortext}</div>;
 }
 export default Error;
--- src/pages/user/login.jsx
+++ src/pages/user/login.jsx
@@ -1,46 +1,80 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import Error from './error'
-function Login(){
-  let error =document.getElementById('error');
-  let sub=function(){
-    let loginForm= document.forms['loginForm'];
-    let userName=loginForm.elements.userName;
-    let psw=loginForm.elements.psw;
-    if(userName.value===''||psw.value===''){
+import React, { useState } from 'react';
+import Error from './error';
+import {Link} from 'react-router-dom'
+function Login() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  function validateForm() {
+    let newErrors = {};
+
+    if (!formData.name) {
+      newErrors.name = 'Name is required';
       window.navigator.vibrate(700);
-      error.className='visible';
+    }
+    if (!formData.password) {
+      newErrors.password = 'Password is required';
+      window.navigator.vibrate(700);
+    } else if (formData.password.length < 8) {
+      newErrors.password = 'Password must be at least 8 characters';
+      window.navigator.vibrate(700);
+    }
+
+    setErrors(newErrors);
+    return !Object.keys(newErrors).length;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (validateForm()) {
+      // submit the form
     }
   }
-    return(
-      <div>
-        <Error errortext={'either of the field is empty'}/>
-<div class="flex flex-col justify-center items-center mt-20">
+
+  return (
+    <div class="flex flex-col justify-center items-center mt-20">
     <h1 className='font-bold text-sm-5 bold text-red-400 text-center'>Login</h1>
-  <form className=" ml-5 mr-5 mt-10 md:mr-20 md:mr-20 md:ml-20" name='loginForm'>
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
+    <Error errortext={!formData.name? errors.name: !formData.password? errors.password: formData.password.length < 8? 'Password must be at least 8 characters':''} />
+    <form onSubmit={handleSubmit} className='ml-5 mr-5 mt-10 md:mr-20 md:mr-20 md:ml-20'>
+      <label className='label'>
+        Name:
+        <input
+         className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+          type="text"
+          value={formData.name}
+          onChange={event => setFormData({ ...formData, name: event.target.value })}
+        />
       </label>
-      <input name='userName' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
-    </div>
-    <div className="mb-6">
-      <label  className= "block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input name='psw' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-      <p className="text-red-500 text-xs italic">Please choose a password.</p>
-    </div>
-    <div className="md:flex items-center justify-between">
-      <button onClick={sub} className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline" type="button" id='sub'>
-        Sign In
-      </button>
-      <p className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-500 ml-5" >
-        Forgot Password?<span className=" text-red-800 text-sm font-bold inline-block"><Link to="../user/register">/Register.</Link></span></p>
-    </div>
-  </form>
-  </div>
-  </div>
-        );
-}
-export default Login;
+      <br />
+      <label className='label'>
+        Email:
+        <input
+        className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+          type="email"
+          value={formData.email}
+          onChange={event => setFormData({ ...formData, email: event.target.value })}
+        />
+        </label>
+        <label className='label'>
+        password:
+        <input
+          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+          type="password"
+          value={formData.password}
+          onChange={event => setFormData({ ...formData, password: event.target.value })}
+        />
+        </label><br/>
+        <button className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline'>Login</button>
+      </form>
+      <p className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-500 ml-5" ><a href="#">
+        Already have an account?</a><span className=" text-red-800 text-sm font-bold inline-block">/<Link to='../user/register'>Register</Link></span></p>
+      </div>
+      )
+  }
+      export default Login;
+      
