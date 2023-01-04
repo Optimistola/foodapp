fe5c1e64d90a66c664a5e73f14fb3cfbdcb84020
Optimistola <yakubshakirudeenolaide2018@gmail.com>
2023-01-04 00:59:07 -0800

    main

--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 function Pages(props) {
   let {page, pageName}=props;
   return (
-    <div className='hero py-20 text-center text-white'>
-      <h1><Link to='/foodapp'>Home</Link>/{pageName}</h1>
+    <div className='hero py-20 text-center text-white flex c0ntent-center justify-center items-center'>
+      <h1 className='font-bold'><Link to='/foodapp'>Home</Link>/{pageName}</h1>
     </div>
   )
 }
