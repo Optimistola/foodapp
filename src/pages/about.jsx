28817b2b851ed15fc139d1bc70d9add4b3d262bd
Optimistola <yakubshakirudeenolaide2018@gmail.com>
2023-01-06 05:50:36 -0800

    main

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -11,7 +11,7 @@ function About(){
               <div className='md:w-[50%] ml-5 lg:w-[40%] ml-10 mr-10'>
                 <img src={about1} className='lg:ml-20'/>
               </div>
-              <div className=' ml-10 mr-10 lg:mt-20 lg:mr-20 lg:ml-20 flex-1'>
+              <div className=' ml-5 mr-5 lg:mt-20 lg:mr-20 lg:ml-20 flex-1'>
                 <h2 className='font-bold text-3xl lg:text-5xl mr-10 lg:mr-20 mt-20'>Food is an important part of a balanced diet</h2>
                 <p className='mt-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque diam pellentesque bibendum non dui volutpat fringilla bibendum. Urna, elit augue urna, vitae feugiat pretium donec id elementum. Ultrices mattis vitae mus risus. Lacus nisi, et ac dapibus sit eu velit in consequat.</p>
               </div>
