609605102027785dc2444a5056080984073399d4
Optimistola <yakubshakirudeenolaide2018@gmail.com>
2023-01-07 09:20:04 -0800

    okay

---
+++ src/Component/shopFile.jsx
@@ -0,0 +1,148 @@
+import product1 from './images/product1.png';
+import product2 from './images/product2.png';
+import product3 from './images/product3.png';
+import product4 from './images/product4.png';
+import product5 from './images/product5.png';
+import product6 from './images/product6.png';
+import product7 from './images/product7.jpg';
+import product8 from './images/product8.png';
+import product9 from './images/product9.jpg';
+import product10 from './images/product10.jpg';
+import product11 from './images/product11.png';
+import product12 from './images/product12.png';
+import product13 from './images/product13.png';
+import product15 from './images/product14.jpg';
+import product14 from './images/product15.png';
+import product16 from './images/product16.png';
+import product17 from './images/product17.png';
+import product18 from './images/product18.png';
+
+    const ProductList=[
+        {
+            id:1,
+            img:product1,
+            key:'Chocolate muffin',
+            price:28,
+            type:'snacks'
+        },
+        {
+            id:2,
+            img:product2,
+            key:'Fresh lime',
+            price:38,
+            type:'food'
+        },
+        {
+            id:3,
+            img:product3,
+            key:'Chicken chup',
+            price:12,
+            type:'food'
+        },
+        {
+            id:4,
+            img:product4,
+            key:'Burger',
+            price:21,
+            type:'snacks'
+        },
+        {
+            id:5,
+            img:product5,
+            key:' Country burger',
+            price:45,
+            type:'snacks'
+        },
+        {
+            id:6,
+            img:product6,
+            key:' sandwitch',
+            price:25,
+            type:'snacks'
+        },
+        {
+            id:7,
+            img:product7,
+            key:' Pounded yam',
+            price:28,
+            type:'food'
+        },
+        {
+            id:8,
+            img:product8,
+            key:' Cheese butter',
+            price:10,
+            type:'snacks'
+        },
+        {
+            id:9,
+            img:product9,
+            key:' Spaghetti',
+            price:25,
+            type:'Food'
+        },
+        {
+            id:10,
+            img:product10,
+            key:'Cake',
+            price:6,
+            type:'snacks'
+        },
+        {
+            id:11,
+            img:product11,
+            key:'Drink',
+            price:23,
+            type:'Drink'
+        },
+        {
+            id:12,
+            img:product12,
+            key:'Pizza',
+            price:43,
+            type:'snacks'
+        },
+        {
+            id:13,
+            img:product13,
+            key:' Chips',
+            price:10,
+            type:'snacks'
+        },
+        {
+            id:14,
+            img:product14,
+            key:' Beans',
+            price:8,
+            type:'Food'
+        },
+        {
+            id:15,
+            img:product15,
+            key:'Burger',
+            price:28,
+            type:'snacks'
+        },
+        {
+            id:16,
+            img:product16,
+            key:'Doughnut',
+            price:16,
+            type:'snacks'
+        },
+        {
+            id:17,
+            img:product17,
+            key:'Coffee',
+            price:12,
+            type:'Drink'
+        },
+        {
+            id:18,
+            img:product18,
+            key:'Italino coffee',
+            price:18,
+            type:'Drink'
+        },
+    ];
+    export default ProductList;
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import {React} from 'react';
 import Pages from './pages'
+import ProductList from '../Component/shopFile';
+import image3 from "../Component/images/image7.png";
+import { BiSearch } from 'react-icons/bi';
+
+
 function Shop(){
     return(
         <div className="hero-text">
               <Pages pageName={'Shop'}/>
-              <div>
-                
+              <div className='mt-20 flex flex-col lg:flex-row-reverse  justify-center lg:mr-10'>
+                <div >
+                    <div className='ml-20 lg:ml-0' >
+                        <div className='flex content-center juistify-center items-center'><input type='search' className='px-3 py-2 shadow bg-red-200'/><span className='text-white px-3 py-3 bg-red-600'><BiSearch/></span></div>
+                        <h3 className='font-bold text-xl'>Category</h3>
+                        <p><input type='checkbox'/>   Sandwiches</p>
+                        <p><input type='checkbox'/>   Burger</p>
+                        <p><input type='checkbox'/>   Chicken chup</p>
+                        <p><input type='checkbox'/>   Drink</p>
+                        <p><input type='checkbox'/>   Thi</p>
+                        <p><input type='checkbox'/>   Non-veg</p>
+                        <p><input type='checkbox'/>   Uncategorized</p>
+                        <div className='hero3 mt-10 mb-10 mr-20 lg:mr-0 text-white'>
+                            <p className='ml-5'>Perfect Taste</p>
+                            <p className='ml-5'>Classic Restaurant</p>
+                            <p className='ml-5 mb-20'>45.00$</p>
+                            <p className='ml-5'>Shop Now</p>
+                      </div>
+                    </div>
+                </div>
+                <div className=' flex flex-col justify-center content-center lg:ml-20'>
+                <h3 className=' mt-10 mb-10 font-bold text-lg ml-10'>Short by</h3>
+              <div className='lg:ml-14  flex flex-wrap justify-center gap-6'>
+              {ProductList.map(items=>(
+                <div className='flex flex-col'>
+                   <img src={items.img} className='w-[180px] h-[180px] md:w-[260px] md:h-[260px] lg:w-[280px] lg:h-[280px] flex-1'/>
+                   <h3 className='text-xl font-bold'>{items.key}</h3>
+                    <p className='text-red-600'>${items.price}.00</p>
+                    </div>
+              ))}
+              </div>
+              </div>
               </div>
         </div>
         );
